df9a6d8b638a833f5b9af3691f4aaf19a0ebc492
Aniket Kumar Tripathi <aniketkumartripathi@gmail.com>
2021-05-26 00:11:55 +0530

    Add templates
    1. Add: book_room, checkout, receptionist
    2. Update catering_service_manager

--- HotelAutomation/base/views.py
+++ HotelAutomation/base/views.py
@@ -7,3 +7,12 @@ def login(request):
 
 def catering_service_manager(request):
     return render(request, 'catering_service_manager.html')
+
+def receptionist(request):
+    return render(request, 'receptionist.html')
+    
+def book_room(request):
+    return render(request, 'book_room.html')
+    
+def checkout(request):
+    return render(request, 'checkout.html')
