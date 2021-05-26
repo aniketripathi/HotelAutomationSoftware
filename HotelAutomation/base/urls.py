227fc56379cc1e15fcd05811094729236c20d179
Aniket Kumar Tripathi <aniketkumartripathi@gmail.com>
2021-05-26 23:06:20 +0530

    Update templates

--- HotelAutomation/base/urls.py
+++ HotelAutomation/base/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
-path('', views.checkout, name='login')
+    path('base/login', views.login, name='login'),
+    path('base/receptionist', views.receptionist, name='receptionist'),
+    path('base/admin', views.admin, name='admin'),
+    path('base/catering_service_manager', views.catering_service_manager, name='catering_service_manager'),
+    path('', RedirectView.as_view(url='base/login', permanent=True)),
+    
 ]
